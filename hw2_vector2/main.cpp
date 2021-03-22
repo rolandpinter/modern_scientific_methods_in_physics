654cb587621fc7035e2937c8d26a2045230c1a41
rolandpinter <pinter.roland.ms@gmail.com>
2021-03-22 17:32:45 +0100

    init commit of vector2 second homework

---
+++ hw2_vector2/main.cpp
@@ -0,0 +1,7 @@
+#include "vector2.hpp"
+
+int main()
+{
+    std::cout << "Init of vector2 homework" << std::endl;
+    return 0;
+}
---
+++ hw2_vector2/vector2.hpp
@@ -0,0 +1,7 @@
+#ifndef vector2_hpp
+#define vector2_hpp
+
+#include <iostream>
+
+
+#endif /* vector2_hpp */
