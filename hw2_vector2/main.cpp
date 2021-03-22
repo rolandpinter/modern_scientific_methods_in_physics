e6f7e512728b2f7ef6389800173a630b3274ab7d
rolandpinter <pinter.roland.ms@gmail.com>
2021-03-22 21:37:12 +0100

    Finished the homework, implemented all the required functionalities, did all the checkings in main

--- hw2_vector2/main.cpp
+++ hw2_vector2/main.cpp
@@ -2,6 +2,65 @@
 
 int main()
 {
-    std::cout << "Init of vector2 homework" << std::endl;
+    // Check 1: create vector2 with given values and output operator for vector2
+    vector2<int> a = {10, 2};
+    vector2<int> b = {3, 4};
+    std::cout << "Initially a vector: " << a << std::endl;
+    std::cout << "Initially b vector: " << b << std::endl;
+
+    // Check 2: Addition in place
+    a+=b;
+    std::cout << "a+=b: " << a << std::endl;
+
+    // Check 3: Substraction in place
+    a-=b;
+    std::cout << "a-=b: " << a << std::endl;
+
+    // Check 4: Multiplication with scalar in place
+    a*=2;
+    std::cout << "a*=2: " << a << std::endl;
+
+    // Check 5: Division with scalar in place
+    a/=2;
+    std::cout << "a/=2: " << a << std::endl;
+
+    // Check 6: Addition of two vectors and returning with result
+    auto sum = a + b;
+    std::cout << "sum = a + b: " << sum << std::endl;
+
+    // Check 7: Substraction of two vectors and returning with result
+    auto diff = a - b;
+    std::cout << "diff = a + b: " << diff << std::endl;
+
+    // Check 8: Multiplication of a vector with a scalar value from right and returning with result
+    auto multRight = a * 2;
+    std::cout << "multRight = a * 2: " << multRight << std::endl;
+
+    // Check 8: Multiplication of a vector with a scalar value from right and returning with result
+    auto multLeft = 2 * a;
+    std::cout << "multLeft = 2 * a: " << multLeft << std::endl;
+
+    // Check 9: Division of a vector with a scalar value and returning with result
+    auto div = a / 2;
+    std::cout << "div = a / 2: " << div << std::endl;
+
+    // Check 10: Length of a given vector
+    double l = length(a);
+    std::cout << "l = length(a): " << l << std::endl;
+
+    // Check 11: Dot product of two vectors and returning with result
+    auto dotP = dot(a,b);
+    std::cout << "dotP = dot(a,b): " << dotP << std::endl;
+
+    // Check 12: Normalise a vector and returning with result
+    auto norm = normalise(a);
+    std::cout << "norm = normalise(a): " << norm << std::endl;
+
+    // Check 13: Read vector components from standard input
+    vector2<int> inputVec{};
+    std::cout << "Enter the two components of a two dimensional vector:" << std::endl;
+    std::cin >> inputVec;
+    std::cout << "inputVec: " << inputVec << std::endl;
+    
     return 0;
 }
--- hw2_vector2/vector2.hpp
+++ hw2_vector2/vector2.hpp
@@ -1,7 +1,107 @@
 #ifndef vector2_hpp
 #define vector2_hpp
 
-#include <iostream>
+#include <iostream> // For IO operations
+#include <cmath>    // For sqrt() function
 
+template<typename T>
+class vector2
+{
+public:
+    T x; // x component of the 2 dimensional vector
+    T y; // y component of the 2 dimensional vector
+
+    // (1) Addition in place
+    vector2<T>& operator+=(vector2<T> const& v){
+        x += v.x;
+        y += v.y;
+        return *this;
+    }
+
+    // (2) Substraction in place
+    vector2<T>& operator-=(vector2<T> const& v){
+        x -= v.x;
+        y -= v.y;
+        return *this;
+    }
+
+    // (3) Multiplication with scalar in place
+    vector2<T>& operator*=(T const& a){
+        x *= a;
+        y *= a;
+        return *this;
+    }
+
+    // (4) Division with scalar in place
+    vector2<T>& operator/=(T const& a){
+        x /= a;
+        y /= a;
+        return *this;
+    }
+
+};
+
+// (1) Addition of two vectors and returning with result
+template<typename T>
+vector2<T> operator+(vector2<T> const& u, vector2<T> const& v){
+    return vector2<T>{u.x + v.x, u.y + v.y};
+}
+
+// (2) Substraction of two vectors and returning with result
+template<typename T>
+vector2<T> operator-(vector2<T> const& u, vector2<T> const& v){
+    return vector2<T>{u.x - v.x, u.y - v.y};
+}
+
+// (3.1) Multiplication of a vector with a scalar value from right and returning with result
+template<typename T>
+vector2<T> operator*(vector2<T> const& u, T const& c){
+    return vector2<T>{u.x * c, u.y * c};
+}
+
+// (3.2) Multiplication of a vector with a scalar value from left and returning with result
+template<typename T>
+vector2<T> operator*(T const& c, vector2<T> const& u){
+    return vector2<T>{c * u.x, c* u.y};
+}
+
+// (4) Division of a vector with a scalar value and returning with result
+template<typename T>
+vector2<T> operator/(vector2<T> const& u, T const& c){
+    return vector2<T>{u.x / c, u.y / c};
+}
+
+// (5) Lenght of a given vector
+template<typename T>
+double length(vector2<T> const& u){
+    return {sqrt(u.x * u.x + u.y * u.y)};
+}
+
+// (6) Dot product of two vectors and returning with result
+template<typename T>
+T dot(vector2<T> const& u, vector2<T> const& v){
+    return {u.x * v.x + u.y * v.y};
+}
+
+// (7) Normalise a vector and returning with result
+template<typename T>
+vector2<double> normalise(vector2<T> const& u){
+    return vector2<double>{u.x / length(u), u.y / length(u)};
+}
+
+// (8) Write out the components of the vector to standard output
+template<typename T>
+std::ostream& operator<<(std::ostream& o, vector2<T> const& u){
+    o << "(" << u.x << ", " << u.y << ")";
+    return o;
+}
+
+// (9) Read vector components from standard input
+template<typename T>
+std::istream& operator>>(std::istream& i, vector2<T>& u){
+    i >> u.x;
+    i >> u.y;
+    return i;
+}
 
 #endif /* vector2_hpp */
